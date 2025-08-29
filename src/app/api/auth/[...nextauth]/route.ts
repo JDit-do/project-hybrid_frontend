c4d355aa3f5846bf790c2f46b26ab12f168ea98c
JDit-do <justdo.ju@gmail.com>
2025-08-29 15:38:56 +0900

    feat: AWS cognito에 Google 인증 추가
    - 커스텀 로그인 페이지에서 Google 인증 진행
    - 로그아웃시 세션 제거

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,14 +4,32 @@ import CognitoProvider from "next-auth/providers/cognito";
 const handler = NextAuth({
   providers: [
     CognitoProvider({
+      issuer: process.env.COGNITO_ISSUER,
       clientId: process.env.COGNITO_CLIENT_ID!,
       clientSecret: process.env.COGNITO_CLIENT_SECRET!,
-      issuer: process.env.COGNITO_ISSUER,
-      authorization: { params: { scope: "openid" } },
+
+      authorization: {
+        params: {
+          scope: "openid",
+          prompt: "select_account",
+          identity_provider: "Google",
+        },
+      },
+
+      checks: ["pkce", "state", "nonce"],
     }),
   ],
   session: {
     strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // 로그인 성공 시 항상 홈으로
+      return baseUrl + "/";
+    },
   },
 });
 
--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -18,7 +18,7 @@ export default function Component() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn("cognito")}>Sign in</button>
     </>
   );
 }
